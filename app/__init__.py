4613feec708ac09e529d466228ee64611d18a10f
Rezende-Fabio <rezendefabio337@gmail.com>
2023-02-21 20:15:01 -0300

    set conection db using SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
 
 def crateApp():
 
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'g4df45h54d5fh64d5h4fg5hg45gf4hh4fg45h45f4gdfg4d5fg4d5s4f'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    DB.init_app(app)
 
     #Blueprint routes Main
     from .blueprints.Main import main
