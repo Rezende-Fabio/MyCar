dae62bfee7d257d99eaca0c855b2fd9568e80edc
Rezende-Fabio <rezendefabio337@gmail.com>
2023-02-21 19:36:24 -0300

    create project

---
+++ Cars.py
@@ -0,0 +1,5 @@
+from app import crateApp
+
+if __name__ == '__main__':
+    crateApp().run()
+    
---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+def crateApp():
+
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'g4df45h54d5fh64d5h4fg5hg45gf4hh4fg45h45f4gdfg4d5fg4d5s4f'
+
+    #Blueprint routes Main
+    from .blueprints.Main import main
+    app.register_blueprint(main)
+
+    return app
